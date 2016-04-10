d6ecbe612d805353d1a5aa61ca20c384016bc68a
Maxine Ellah <maxine.ellah@gmail.com>
2016-04-10 19:02:30 +1200

    try to add background image

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+var bananaStats = require('./views/bananaStats.hbs')
 var request = require('superagent')
 var $ = require('jquery')
 
-console.log('bundled js is working')
 
-
-
-$("button").click(function(){
-    var x = $("form").serializeArray();
-    $.each(x, function(i, field){
-        $("#results").append(field.name + ":" + field.value + " ");
-    });
-});
+// request
+//   .get('http://localhost:3000/')
+//   .end(function(err, res){
+//   console.log('this is res.body', res.body)
+//   document.body.innerHTML = bananaStats({banana: res.body})
+    $("button").click(function(){
+      var x = $("form").serializeArray();
+      $.each(x, function(i, field){
+          $("#results").append(field.name + ":" + field.value + " ");
+      });
+  });
+    
+// });
 
