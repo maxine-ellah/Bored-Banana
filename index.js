e865a553c99fca7dbf9db1bd48440031e88198f8
Maxine Ellah <maxine.ellah@gmail.com>
2016-04-10 17:32:48 +1200

    add .serializeArray() to html

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var request = require('superagent')
+var $ = require('jquery')
 
 console.log('bundled js is working')
+
+
+
+$("button").click(function(){
+    var x = $("form").serializeArray();
+    $.each(x, function(i, field){
+        $("#results").append(field.name + ":" + field.value + " ");
+    });
+});
+
