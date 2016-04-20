b38f392aa07be653f67bf1e96202bd967ad23363
maxine-ellah <email": null>
2016-04-20 12:25:32 +1200

    start working on timer.js set interval function

--- client/apptimer.js
+++ client/apptimer.js
@@ -9846,5 +9846,28 @@ return jQuery;
 var $ = require("jquery")
 
 
+var timeEntered = $("#time").text()
+
+// console.log("timeEntered: ", timeEntered)
+
+// var time = document.querySelector('#time').textContent
+
+// console.log("time: ", time)
+
+var remainingTime =
+
+
+function calculateTimeLeft(timeEntered) {
+  // var a = moment().format()
+  // var b = moment(Number(timeEntered))
+  // var difference = a.diff(b, 'hours')
+  // console.log(difference)
+  // var now = moment();
+  var timeEnteredMoment = moment(timeEntered);
+  var inSevenDays = moment([2016, 3, 22])
+  var difference = timeEnteredMoment.to(inSevenDays)
+  console.log('difference: ', difference)
+  return difference
+}
 
 },{"jquery":1}]},{},[2]);
