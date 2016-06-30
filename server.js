39a24a70ae1cc2113264b145d050be5a789e4f1e
Maxine Ellah <maxine.ellah@gmail.com>
2016-06-30 12:01:17 +1200

    add session config at top of server.js file

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('client'));
-
+app.use(session({ secret: 'cinnamon bun', resave: false, saveUninitialized: false }))
 
 
 app.get('/', function (req, res) {
