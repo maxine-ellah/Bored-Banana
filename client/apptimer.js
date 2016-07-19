84e1cde281a46b720dc110916b37d9c8662e43fa
Maxine Ellah <maxine.ellah@gmail.com>
2016-07-19 16:14:29 +1200

    put expiration checker if statement back inside timer function

--- client/apptimer.js
+++ client/apptimer.js
@@ -13866,26 +13866,16 @@ var dateBought = moment(document.getElementById('time').innerHTML, "YYYY-MM-DD")
 
 var endTime = moment(dateBought).add(7, 'day')
 
-
-
 function timerFunction() {
-if (!document.getElementById('time')) {
-  clearInterval(intervalID)
-}
+  if (!document.getElementById('time')) {
+    clearInterval(intervalID)
+  }
 
+  var days = moment.duration(endTime - moment()).days()
+  var hours = moment.duration(endTime - moment()).hours()
+  var minutes = moment.duration(endTime - moment()).minutes()
+  var seconds = moment.duration(endTime - moment()).seconds()
 
-
-var days = moment.duration(endTime - moment()).days()
-var hours = moment.duration(endTime - moment()).hours()
-var minutes = moment.duration(endTime - moment()).minutes()
-var seconds = moment.duration(endTime - moment()).seconds()
-
-expirationAlert(days, hours, minutes, seconds, intervalID)
-
-$("#timeDisplay").text(days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ")
-}
-
-function expirationAlert(days, hours, minutes, seconds, intervalID) {
   if (days <= 1 && hours <= 1 && minutes <= 1 && seconds <= 1) {
     days = 0
     hours = 0
@@ -13894,6 +13884,13 @@ function expirationAlert(days, hours, minutes, seconds, intervalID) {
     clearInterval(intervalID)
     alert("Your bananas are old, put them in the freezer.")
   }
+
+  displayTimer(days, hours, minutes, seconds)
+
+}
+
+function displayTimer(days, hours, minutes, seconds){
+  document.getElementById('timeDisplay').innerHTML = (days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ")
 }
 
 },{"jquery":1,"moment":2}]},{},[3]);
--- src/timer.js
+++ src/timer.js
@@ -8,26 +8,16 @@ var dateBought = moment(document.getElementById('time').innerHTML, "YYYY-MM-DD")
 
 var endTime = moment(dateBought).add(7, 'day')
 
-
-
 function timerFunction() {
-if (!document.getElementById('time')) {
-  clearInterval(intervalID)
-}
+  if (!document.getElementById('time')) {
+    clearInterval(intervalID)
+  }
 
+  var days = moment.duration(endTime - moment()).days()
+  var hours = moment.duration(endTime - moment()).hours()
+  var minutes = moment.duration(endTime - moment()).minutes()
+  var seconds = moment.duration(endTime - moment()).seconds()
 
-
-var days = moment.duration(endTime - moment()).days()
-var hours = moment.duration(endTime - moment()).hours()
-var minutes = moment.duration(endTime - moment()).minutes()
-var seconds = moment.duration(endTime - moment()).seconds()
-
-expirationAlert(days, hours, minutes, seconds, intervalID)
-
-$("#timeDisplay").text(days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ")
-}
-
-function expirationAlert(days, hours, minutes, seconds, intervalID) {
   if (days <= 1 && hours <= 1 && minutes <= 1 && seconds <= 1) {
     days = 0
     hours = 0
@@ -36,4 +26,11 @@ function expirationAlert(days, hours, minutes, seconds, intervalID) {
     clearInterval(intervalID)
     alert("Your bananas are old, put them in the freezer.")
   }
+
+  displayTimer(days, hours, minutes, seconds)
+
+}
+
+function displayTimer(days, hours, minutes, seconds){
+  document.getElementById('timeDisplay').innerHTML = (days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ")
 }
